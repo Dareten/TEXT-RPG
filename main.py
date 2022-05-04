886fb0d6b9e81deafc78ec6878930b2824847134
Dareten <Daretens@gmail.com>
2022-05-04 12:16:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from telethon import TelegramClient, events, Button
+
+import config
+
+scope = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/spreadsheets']
+creds = ServiceAccountCredentials.from_json_keyfile_name('SIPI.json', scope)
+google_client = gspread.authorize(creds)
+sheet = google_client.open('SIPI').worksheet('MAIN')
+bot = TelegramClient('SIPI', config.api_code, config.api_hash,
+                     auto_reconnect=True).start(bot_token=config.token)
+
+
+@bot.on(events.NewMessage(pattern=r'^/start$'))
+async def start(event):
+    if event.text != "/start":
+        await event.reply("Используйте команду /start")
+    else:
+        keyboard = [
+            [Button.text("a")],
+            [Button.text("b")]
+        ]
+        await event.respond("Добро пожаловать в Текстовая игра жанра RPG.", buttons=keyboard)
+
+
+bot.run_until_disconnected()
